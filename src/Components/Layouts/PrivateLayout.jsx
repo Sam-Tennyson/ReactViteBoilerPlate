73c9ddf59d92151255447c1d97c7741e952d9b9c
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2023-05-21 12:37:28 +0530

    env-setup-for-api-integration

--- src/Components/Layouts/PrivateLayout.jsx
+++ src/Components/Layouts/PrivateLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import './style.scss'
+
 const PrivateLayout = ({children}) => {
   return (
     <>
+      <h2>Boiler place integrated successfully</h2>
       <div className='private-screen'>
         {children}
       </div>
+      <Outlet />
     </>
   )
 }
--- src/Routes/PublicRoute.jsx
+++ src/Routes/PublicRoute.jsx
@@ -1,10 +1,17 @@
 import { ROUTE_CONSTANTS } from "../Shared/Routes";
+import Login from "../Views/Authentication/Login";
+import Signup from "../Views/Authentication/Signup";
 
 export const PublicRoute = [
-    // {
-    //     path: ROUTE_CONSTANTS.DASHBOARD,
-    //     element: <BasicView />,
-    //     title: "BasicView",
-    // },
+    {
+        path: ROUTE_CONSTANTS.LOGIN,
+        element: <Login />,
+        title: "Login"  
+    },
+    {
+        path: ROUTE_CONSTANTS.SIGNUP,
+        element: <Signup />,
+        title: "Signup"  
+    },
 ]
 
